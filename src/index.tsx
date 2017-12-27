e6498063baedc1bbf3a280f169f6e5fd71352954
Riku Rouvila <riku.rouvila@gmail.com>
2017-12-27 00:06:41 +0000

    reload features when view changes in github

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,33 @@
 import { UnauthorizedError } from "./api";
-import diffSelect from './features/diff-select'
+import diffSelect from "./features/diff-select";
 import { createContext } from "./lib/context";
 import { isPRView } from "./lib/location";
 
-(async function run() {
-  if(!isPRView(document.location)) {
-    return
+async function run() {
+  if (!isPRView(document.location)) {
+    return;
   }
-  const context = await createContext(document.location)
-  diffSelect(context)
+  const context = await createContext(document.location);
+  await diffSelect(context);
+}
 
-})().catch(err => {
-  if(err instanceof UnauthorizedError) {
-    const token = window.prompt('Please enter an access token')
-    if(token) {
-      window.localStorage.setItem('TODO_token', token);
-      window.location.reload()
+const observer = new MutationObserver(() => {
+  run().catch(err => {
+    if (err instanceof UnauthorizedError) {
+      const token = window.prompt("Please enter an access token");
+      if (token) {
+        window.localStorage.setItem("TODO_token", token);
+        window.location.reload();
+      }
     }
-  }
-
-  console.log(err);
+    console.log(err);
+  });
 });
+
+// Start observing the target node for configured mutations
+const el = document.querySelector("#js-repo-pjax-container");
+if (el) {
+  observer.observe(el, {
+    childList: true
+  });
+}
