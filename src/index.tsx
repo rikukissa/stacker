c82213d622769403b9642c78ca38f3f9bfbf2e95
Riku Rouvila <riku.rouvila@gmail.com>
2018-01-03 23:40:19 +0000

    add initial setup for config popup

--- src/index.tsx
+++ src/index.tsx
@@ -1,55 +1,15 @@
-import { UnauthorizedError } from "./api";
-import diffSelect from "./features/diff-select";
-import fadeOutUnrelatedCommits from "./features/fade-out-unrelated-commits";
-import mergeWarning from "./features/merge-warning";
-import parentPRSelect from "./features/parent-pr-select";
-import showStackingInList from "./features/show-stacking-in-list";
-import { getConfig, setConfig } from "./lib/config";
-import { createContext } from "./lib/context";
-import { isPRView } from "./lib/location";
+import initializeExtension from "./extension";
+import initializePopup from "./popup";
 
-function promptForToken() {
-  const token = window.prompt("Please enter an access token");
-  if (token) {
-    setConfig({ token });
-    window.location.reload();
+declare global {
+  // tslint:disable-next-line interface-name
+  interface Window {
+    STACKER_POPUP: boolean;
   }
 }
 
-async function run() {
-  if (!isPRView(document.location)) {
-    return;
-  }
-
-  const token = getConfig().token;
-
-  if (!token) {
-    return promptForToken();
-  }
-
-  const context = await createContext(document.location);
-  try {
-    await diffSelect(context);
-    await showStackingInList(context);
-    await parentPRSelect(context);
-    await fadeOutUnrelatedCommits(context);
-    await mergeWarning(context);
-  } catch (err) {
-    if (err instanceof UnauthorizedError) {
-      promptForToken();
-    }
-    // tslint:disable-next-line no-console
-    console.log(err);
-  }
+if (document.getElementById('stacker-popup')) {
+  initializePopup();
+} else {
+  initializeExtension();
 }
-
-const observer = new MutationObserver(run);
-const el = document.querySelector("#js-repo-pjax-container");
-
-if (el) {
-  observer.observe(el, {
-    childList: true
-  });
-}
-
-run();
---
+++ src/popup.tsx
@@ -0,0 +1,9 @@
+import { h } from "jsx-dom";
+
+export default function initialize() {
+  document.body.appendChild(
+    <div>
+      <h1>moro</h1>
+    </div>
+  );
+}
