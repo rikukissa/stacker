320712fb384e95be939359467c5aa155b872180c
Riku Rouvila <riku.rouvila@gmail.com>
2018-01-07 10:13:05 +0000

    Travis configuration & test refactoring (#33)
    * add travis, abstract commonly used pieces from first e2e to test utility
    
    * test against built plugin on CI
    
    * add google-chrome-beta travis addon
    
    * use different executable path for chrome on CI
    
    * use different path for chrome binary
    
    * try upgrading node & addon syntax
    
    * add dist
    
    * now just guessing..
    
    * use different environment variable to detect CI
    
    * remove manual executable path

---
+++ src/tests/utils.ts
@@ -0,0 +1,30 @@
+import { join } from "path";
+import * as puppeteer from "puppeteer";
+
+const PLUGIN_PATH = process.env.CI
+  ? join(__dirname, "../../build")
+  : join(__dirname, "../../dev");
+
+export async function getTextContent(element: puppeteer.JSHandle) {
+  const property = await element.getProperty("textContent");
+  return (await property).jsonValue();
+}
+
+export async function createBrowser() {
+  const browser = await puppeteer.launch({
+    args: [
+      `--disable-extensions-except=${PLUGIN_PATH}`,
+      `--load-extension=${PLUGIN_PATH}`,
+      `--ignore-certificate-errors`
+    ],
+    headless: false
+  });
+
+  const page = await browser.newPage();
+
+  page.on("dialog", async (dialog: puppeteer.Dialog) => {
+    await dialog.accept(process.env.GITHUB_TOKEN);
+  });
+
+  return { browser, page };
+}
