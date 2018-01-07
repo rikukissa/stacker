275bfd9f83617e80a37be576973f75af753e0830
Riku Rouvila <riku.rouvila@gmail.com>
2018-01-07 22:24:54 +0000

    add e2e util for logging in

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -28,3 +28,28 @@ export async function createBrowser() {
 
   return { browser, page };
 }
+
+export async function login(page: puppeteer.Page) {
+  if (!(process.env.GITHUB_USERNAME && process.env.GITHUB_PASSWORD)) {
+    throw new Error(
+      "Missing required environment variables GITHUB_USERNAME or GITHUB_PASSWORD"
+    );
+  }
+
+  await page.goto("https://github.com/login");
+  await page.waitFor(".auth-form-body");
+
+  const $username = await page.$("#login_field");
+  const $password = await page.$("#password");
+  const $submit = await page.$("input[type=submit]");
+
+  if (!($username && $password && $submit)) {
+    throw new Error("Submit button or username/password fields not found");
+  }
+
+  await $username.type(process.env.GITHUB_USERNAME);
+  await $password.type(process.env.GITHUB_PASSWORD);
+
+  await $submit.click();
+  await page.waitForNavigation();
+}
